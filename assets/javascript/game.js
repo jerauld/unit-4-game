cbe08dd7230bfd3493133cc2c6e38a0928b72b12
jerauld <25723428+jerauld@users.noreply.github.com>
2018-04-30 08:48:25 -0700

    added intro click to start, corrected score gem spacing, retimed intro

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,20 @@ $(document).ready(function () {
 
     var wins = 0;
     var losses = 0;
-    $("#win-count").text(wins);
-    $("#loss-count").text(wins);
-    $("#win-header").text("wins");
-    $("#loss-header").text("losses");
+   
+    $("#clickToStart").text("Click anywhere to start");
+    new Audio("assets/audio/LOZ_Stairs.wav").play();
+    
+    $("#clickToStart").on("click", function(){
+        $("#win-count").text(wins);
+        $("#loss-count").text(wins);
+        $("#win-header").text("wins");
+        $("#loss-header").text("losses");
+        $("#number-to-guess").text(targetNumber);
+        $("#user-total-score").text(counter);
+        $("#clickToStart").addClass("isHidden");
+        generateRupees();
+    });
 
     var getRandomIntInclusive = function (min, max) {
         min = Math.ceil(min);
@@ -13,8 +23,8 @@ $(document).ready(function () {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    var txt = ["Let's play a number matching game!", "I have a magic number.", "Click on the four rupees to match my magic number.", "The value of each rupee is a secret to everybody.", "Click on a rupee to get started..."];
-    var resultTXT = ["You win! Click to play again!", "You lose! Click to try again!"]
+    var txtIntro = ["Let's play a number matching game!", "I have a magic number.", "Click on the four rupees to match my magic number.", "The value of each rupee is a secret to everybody.", "Click on a rupee to get started..."];
+    var txtResult = ["You win! Click anywhere to play again!", "You lose! Click anywhere to try again!"]
     var txtFinished = true;
     var roundFinished = false;
     var j = 0;
@@ -35,7 +45,7 @@ $(document).ready(function () {
     };
 
     $(document).on("click", function () {
-        if (j >= txt.length) {
+        if (j >= txtIntro.length) {
             if (roundFinished && txtFinished) {
                 resetGame();
             } else {
@@ -43,14 +53,14 @@ $(document).ready(function () {
             }
         } else if (txtFinished) {
             $("#demo").text("");
-            typeWriter(txt, j);
+            typeWriter(txtIntro, j);
             txtFinished = false;
             j++;
         }
         
         if (j === 1) {
             $("#skip-intro").text("Click here to skip intro");
-        } else if (j >= txt.length) {
+        } else if (j >= txtIntro.length) {
             $("#skip-intro").addClass("isHidden");
         }
     });
@@ -60,23 +70,21 @@ $(document).ready(function () {
             txtFinished = false;
             $("#demo").text("");
             j = 4
-            typeWriter(txt, j);
+            typeWriter(txtIntro, j);
             j++
             $("#skip-intro").addClass("isHidden");
         }
     });
 
     var targetNumber = getRandomIntInclusive(19, 120);
-    $("#number-to-guess").text(targetNumber);
     var counter = 0;
-    $("#user-total-score").text(counter);
 
 
-    var gemImages = ["assets/images/gem-purple.png", "assets/images/gem-red.png", "assets/images/gem-blue.png", "assets/images/gem-green.png"]
+    var rupeeImageArray = ["assets/images/gem-purple.png", "assets/images/gem-red.png", "assets/images/gem-blue.png", "assets/images/gem-green.png"]
 
-    function generateCrystals() {
+    function generateRupees() {
 
-        var numberOptionsGenerator = function () {
+        var generateNumberOptions = function () {
             while (numberOptions.length < 4) {
                 var randomNumber = getRandomIntInclusive(2, 12);
                 if (!numberOptions.includes(randomNumber)) {
@@ -87,21 +95,21 @@ $(document).ready(function () {
 
         var numberOptions = [];
 
-        numberOptionsGenerator();
+        generateNumberOptions();
 
-        for (var i = 0; i < gemImages.length; i++) {
+        for (var i = 0; i < rupeeImageArray.length; i++) {
             var imageCrystal = $("<img>");
             imageCrystal.addClass("crystal-image");
-            imageCrystal.attr("src", gemImages[i]);
+            imageCrystal.attr("src", rupeeImageArray[i]);
             imageCrystal.attr("data-crystalvalue", numberOptions[i]);
             $("#crystals").append(imageCrystal);
         }
     }
 
-    generateCrystals();
+    
 
     $(document).on("click", ".crystal-image", function () {
-        if ((j >= txt.length) && (txtFinished) && (roundFinished === false)) {
+        if ((j >= txtIntro.length) && (txtFinished) && (roundFinished === false)) {
             $("#demo").text("");
             new Audio("assets/audio/LOZ_Get_Rupee.wav").play();
             var crystalValue = ($(this).attr("data-crystalvalue"));
@@ -111,7 +119,7 @@ $(document).ready(function () {
 
             if (counter === targetNumber) {
                 new Audio("assets/audio/LOZ_Secret.wav").play();
-                typeWriter(resultTXT, 0);
+                typeWriter(txtResult, 0);
                 wins++;
                 $("#win-count").text(wins);
                 txtFinished = false;
@@ -121,7 +129,7 @@ $(document).ready(function () {
                 new Audio("assets/audio/LOZ_Link_Die.wav").play();
                 losses++;
                 $("#loss-count").text(losses);
-                typeWriter(resultTXT, 1);
+                typeWriter(txtResult, 1);
                 txtFinished = false;
                 roundFinished = true;
 
@@ -140,7 +148,7 @@ $(document).ready(function () {
         $("#user-total-score").text(counter);
         //Generate New Crystals
         $( ".crystal-image" ).remove();
-        generateCrystals();
+        generateRupees();
         //Round is Finished
         roundFinished = false;
         
